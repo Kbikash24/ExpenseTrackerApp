03f99c2de292df2e8f0b81c71a36206627153f4f
Kbikash24 <bikashk11@gmail.com>
2023-10-04 17:09:55 +0530

    email-Verification

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MainPage.css";
 import { useContext } from "react";
 import AuthContext from "../Context/AuthContext";
@@ -12,25 +12,77 @@ const MainPage = () => {
     AuthCtx.logout();
   };
 
+  const [error, setError] = useState("");
+  const isLoggedIn = localStorage.getItem("token");
+
+  const verifyHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDz0pafMT_oDg5zZ7kpQLMb5kEhGfW0ZmA",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            requestType: "VERIFY_EMAIL",
+            idToken: localStorage.getItem("token"),
+        
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        } 
+      );
+       
+      if (res.ok) {
+        const data = await res.json();
+        console.log(data);
+      } else {
+        const data = await res.json();
+        if (data && data.error && data.error.message) {
+          setError("Verification mail not sent... try again" + data.error.message);
+        } else {
+          setError("Some error occurred!! Please try again..");
+        }
+      }
+    } catch (err) {
+      console.error("Some error in sending verification mail - " + err);
+    }
+  };
+
   return (
     <div className="headerr">
-      {AuthCtx.isLoggedIn ? (
+      {isLoggedIn ? (
         <>
-          {" "}
           <div className="title">
             <p className="heading">Welcome to Expense Tracker app</p>
-            <div className="buttons"><div className="profile">profile</div>
-            <button className="Logout" onClick={handleLogout}>
-            
-              Logout
-            </button></div>
+            <div className="buttons">
+              <div className="profile">profile</div>
+              <button className="Logout" onClick={handleLogout}>
+                Logout
+              </button>
+            </div>
           </div>
           <div className="display">
-           <p className="p">Your Profile is incomplete.<div className="profile-link" onClick={()=>{navigate('/profile')}}>Complete now</div></p>
+            <p className="p">
+              Your Profile is incomplete.
+              <div className="profile-link" onClick={() => navigate("/profile")}>
+                Complete now
+              </div>
+            </p>
           </div>
-          
+          <div className="display">
+            <p className="p">
+              Your email is not verified.
+              <div className="profile-link" onClick={verifyHandler}>
+                Verify now
+              </div>
+            </p>
+          </div>
+          <p>{error}</p>
         </>
-      ) : ( navigate("/login")
+      ) : (
+        navigate("/login")
+        // You can uncomment and modify this section if needed
         // <div className="headers">
         //   <button className="Login" onClick={() => navigate("/login")}>
         //     Login
